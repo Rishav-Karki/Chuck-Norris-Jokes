33f689114f08cb3576d069ccf25d7fb0503c776b
@Rishav-Karki <krishavrkarki@gmail.com>
2022-10-10 22:10:03 -0400

    closes #4

---
+++ src/main/java/com/springu/chucknorrisjokes/controllers/JokeController.java
@@ -0,0 +1,25 @@
+package com.springu.chucknorrisjokes.controllers;
+
+import com.springu.chucknorrisjokes.services.JokesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokesService jokesService;
+
+    //@Autowired
+    public JokeController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokesService.getJoke());
+        return "index";
+    }
+
+}
